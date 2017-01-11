632bd1d62cbcea1434ae5719d14a06a5a108d30e
Luc <lucas@hardbarger.com>
2017-01-11 11:00:55 -0500

    Add guestbook and guestbook entries

---
+++ src/CleanArchitecture.Core/Entities/GuestBook.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArchitecture.Core.SharedKernel;
+
+namespace CleanArchitecture.Core.Entities
+{
+    public class Guestbook : BaseEntity
+    {
+		public string Name { get; set; }
+		public IList<GuestbookEntry> Entries { get; } = new List<GuestbookEntry>();
+    }
+}
---
+++ src/CleanArchitecture.Web/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArchitecture.Core.Entities;
+
+namespace CleanArchitecture.Web.ViewModels
+{
+    public class HomePageViewModel
+    {
+		public string GuestbookName { get; set; }
+		public IList<GuestbookEntry> PreviousEntries { get; }
+    }
+}
